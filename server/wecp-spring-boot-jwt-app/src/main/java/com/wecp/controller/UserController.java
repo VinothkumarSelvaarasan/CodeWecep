bd454fda5168e845f78d398a7a72a097ed536693
username <wecp-dummy@gmail.com>
2023-07-07 10:11:44 +0000

    Initial Change

--- server/wecp-spring-boot-jwt-app/src/main/java/com/wecp/controller/UserController.java
+++ server/wecp-spring-boot-jwt-app/src/main/java/com/wecp/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -56,4 +57,10 @@ public class UserController {
 		return ResponseEntity.ok(users);
 	}
 
+}
+
+@GetMapping("/readData")
+public String getData()
+{
+	return "Hai Welcome to Git Hub";
 }
